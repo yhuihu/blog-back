5e9bb7debf00c26e4d7d852e72377cc069cb6347
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-08-26 17:32:17 +0800

    bug修复

--- src/main/java/com/blog/blogBack/config/dao/SessionFactoryConfiguration.java
+++ src/main/java/com/blog/blogBack/config/dao/SessionFactoryConfiguration.java
@@ -1,9 +1,7 @@
 package com.blog.blogBack.config.dao;
 
 import com.blog.blogBack.framework.mapper.IMyMapper;
-import com.github.pagehelper.PageHelper;
 import com.zaxxer.hikari.HikariDataSource;
-import org.apache.ibatis.plugin.Interceptor;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,17 +29,6 @@ public class SessionFactoryConfiguration {
         sqlSessionFactoryBean.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
         // 设置typeAlias 包扫描路径
         sqlSessionFactoryBean.setTypeAliasesPackage("com.blog.blogBack.entity");
-
-        //配置分页插件，详情请查阅官方文档
-        PageHelper pageHelper = new PageHelper();
-        Properties properties = new Properties();
-        //properties.setProperty("pageSizeZero", "true");//分页尺寸为0时查询所有纪录不再执行分页
-        properties.setProperty("reasonable", "true");//页码<=0 查询第一页，页码>=总页数查询最后一页
-        properties.setProperty("supportMethodsArguments", "true");//支持通过 Mapper 接口参数来传递分页参数
-        pageHelper.setProperties(properties);
-
-        //添加插件
-        sqlSessionFactoryBean.setPlugins(new Interceptor[]{pageHelper});
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
         sqlSessionFactoryBean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
