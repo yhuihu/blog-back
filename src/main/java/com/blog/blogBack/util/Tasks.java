48abb7f0a464932f7e4360e821605d4af8f3d51a
yhuihu <35516186+yhuihu@users.noreply.github.com>
2019-09-01 11:25:26 +0800

    更新每周备份数据库并发送到管理员邮箱

--- src/main/java/com/blog/blogBack/util/Tasks.java
+++ src/main/java/com/blog/blogBack/util/Tasks.java
@@ -4,11 +4,12 @@ import com.blog.blogBack.entity.SendComment;
 import com.blog.blogBack.service.SendCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.mail.MessagingException;
+import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -18,31 +19,52 @@ public class Tasks {
     @Autowired
     SendCommentService sendCommentService;
     @Autowired
-    JavaMailSender javaMailSender;
+    EmailTool emailTool;
     @Value("${spring.datasource.username}")
     private String dataBaseUserName;
     @Value("${spring.datasource.password}")
     private String dataBasePassword;
+    @Value("${mysql.dump.location}")
+    private String savePath;
+    @Value("${databaseName}")
+    private String dataBaseName;
+    @Value("${email.adminEmail}")
+    private String to;
     @Scheduled(cron = "0 00 08 * * ? ")
     public void sendEmail() {
-        System.out.println("现在是:"+new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
-        StringBuilder sendText= new StringBuilder();
-        String blogUrl="该条博客地址：http://blog.yhhu.xyz/#/blog/";
-        List<SendComment> list=sendCommentService.findAll();
-        for (SendComment item :list) {
+        System.out.println("现在是:" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + ",开始发送邮件");
+        StringBuilder sendText = new StringBuilder();
+        String blogUrl = "该条博客地址：http://blog.yhhu.xyz/#/blog/";
+        List<SendComment> list = sendCommentService.findAll();
+        for (SendComment item : list) {
             sendText.append(item.getReaderName()).append("在《").append(item.getTitle()).append("》下回复----")
                     .append(item.getContent()).append("----").append(blogUrl)
                     .append(item.getBlogId()).append("\n");
             sendCommentService.deleteById(item.getId());
         }
-        SimpleMailMessage message = new SimpleMailMessage();
-        String from = "1357958736@qq.com";
-        message.setFrom(from);
-        message.setTo(from);
-        message.setSubject("Blog评论");
-        message.setText(String.valueOf(sendText));
-        javaMailSender.send(message);
+        if(list.size()>0) {
+            emailTool.sendSimpleMail(to,"Blog每日评论",String.valueOf(sendText));
+        }
         System.out.println("今天的邮件已经发送完成了");
     }
 
+    //每周一备份一次
+    @Scheduled(cron = "0 00 00 ? * MON ")
+//    @Scheduled(cron = "0 23 11 * * ? ")
+    public void saveDataBase() throws IOException, MessagingException {
+        System.out.println("现在是:" + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + ",开始备份数据库");
+        File file = new File(savePath);
+        if (!file.exists())
+            file.mkdirs();
+        String window_cmd = "cmd /c mysqldump -u" + dataBaseUserName + " -p" + dataBasePassword + " vueblog >"
+                + savePath + new SimpleDateFormat("yyyy-MM-dd").format(new Date())
+                + ".sql";
+        String fileUrl=savePath + new SimpleDateFormat("yyyy-MM-dd").format(new Date()) + ".sql";
+        String[] centOS_cmd = {"/bin/sh","-c","mysqldump -u" + dataBaseUserName + " -p"
+                + dataBasePassword + " " + dataBaseName + " > "
+                + fileUrl};
+        Process process = Runtime.getRuntime().exec(centOS_cmd);
+        System.out.println("开始将数据库发送到管理员邮箱。");
+        emailTool.sendAttachmentsMail(to,"数据库备份","当前时间为："+ new SimpleDateFormat("yyyy-MM-dd HH:mm").format(new Date()),fileUrl);
+    }
 }
